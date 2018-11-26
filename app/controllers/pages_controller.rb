c6e4457782fff785750b42790685642260f9bdf5
hisayohorie <hisayohorie@gmail.com>
2018-11-26 13:02:13 -0500

    Adding kitten page method and route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,9 @@ class PagesController < ApplicationController
   def about
     @header = "About this"
   end
+
+  def kitten
+    requested_size = params[:size]
+    @kitten_url = "http://placekitten.com/#{requested_size}/#{requested_size}"
+  end
 end
